8348efd6310377a58434bd51711fd1a856f3d04b
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-02-12 22:14:18 +0530

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 
 //pages
 import Home from './pages/Home';
@@ -7,10 +7,17 @@ import About from './pages/About';
 function App() {
   return (
     <BrowserRouter>
+    <header>
+      <nav>
+        <h1>Jobarouter</h1>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="about">About</NavLink>
+      </nav>
+    </header>
       <main>
         <Routes>
           <Route index element={<Home />} />
-          <Route path = "/about" element={<About />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </main>
     </BrowserRouter>
