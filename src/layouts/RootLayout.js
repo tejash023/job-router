a6afd820134f259ec81be98fef586f07b59c6d10
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-02-12 22:32:13 +0530

    added createBrowserRouter method due to react router v6.4

---
+++ src/layouts/RootLayout.js
@@ -0,0 +1,15 @@
+import { NavLink } from 'react-router-dom'
+
+export default function RootLayout() {
+  return (
+    <div className='root-layout'>
+      <header>
+        <nav>
+          <h1>Jobarouter</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+    </div>
+  )
+}
