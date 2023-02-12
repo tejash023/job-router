ccd0f6becb564d26851a60d0c4f61982a6e1b0ed
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-02-12 22:50:41 +0530

    add elements to faq and contact

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Home from './pages/Home';
 import About from './pages/About';
 import RootLayout from './layouts/RootLayout';
 import HelpLayout from './layouts/HelpLayout';
+import FAQ from './pages/help/FAQ'
+import Contact from './pages/help/Contact'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -17,8 +19,8 @@ const router = createBrowserRouter(
       <Route index element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path='help' element={<HelpLayout />}>
-        <Route path='faq' />
-        <Route path='contact' />
+        <Route path='faq' element={<FAQ />}/>
+        <Route path='contact' element={<Contact />}/>
       </Route>
     </Route>
   )
