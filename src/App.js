82807f013aee984136b382e7d2bb7adf04f77d01
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-02-12 22:11:18 +0530

    added home and about components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
+//pages
+import Home from './pages/Home';
+import About from './pages/About';
+
 function App() {
   return (
     <BrowserRouter>
       <main>
         <Routes>
           <Route index element={<Home />} />
-          <Route index element={<Home />} />
+          <Route path = "/about" element={<About />} />
         </Routes>
       </main>
     </BrowserRouter>
