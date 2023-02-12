e177b721c7c7e59b73387a32a2561f7be1178c7a
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-02-12 22:18:09 +0530

    added css for navigation bar

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,9 +4,9 @@ export default function About() {
   return (
     <div className='about'>
       <h2>About Us</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Lorem ipsum dolor sit amet. Ab similique vero in quia eligendi eum quas saepe. Qui cumque reiciendis id provident perspiciatis sit repudiandae tempore quo enim rerum vel accusamus dolores. Ut ullam deserunt non ullam facere qui labore animi aut ducimus omnis.</p>
+      <p>Lorem ipsum dolor sit amet. Ab similique vero in quia eligendi eum quas saepe. Qui cumque reiciendis id provident perspiciatis sit repudiandae tempore quo enim rerum vel accusamus dolores. Ut ullam deserunt non ullam facere qui labore animi aut ducimus omnis.</p>
+      <p>Lorem ipsum dolor sit amet. Ab similique vero in quia eligendi eum quas saepe. Qui cumque reiciendis id provident perspiciatis sit repudiandae tempore quo enim rerum vel accusamus dolores. Ut ullam deserunt non ullam facere qui labore animi aut ducimus omnis.</p>
       
     </div>
   )
