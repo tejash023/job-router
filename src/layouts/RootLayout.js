aab414691e54e108a91605b35981355c441b2964
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-02-13 13:53:18 +0530

    updated root layouts

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -8,8 +8,9 @@ export default function RootLayout() {
           <h1>Jobarouter</h1>
           <NavLink to="/">Home</NavLink>
           <NavLink to="about">About</NavLink>
-          <NavLink to="help">Help</NavLink>
           <NavLink to="careers">Careers</NavLink>
+          <NavLink to="help">Help</NavLink>
+          
         </nav>
       </header>
       <main>
